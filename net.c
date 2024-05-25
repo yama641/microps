b84a68596f68e176c448f0e465afbeac564e1293
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 04:36:13 +0900

    Completion of step0

---
+++ net.c
@@ -0,0 +1,37 @@
+#include "platform.h"
+
+#include "util.h"
+
+int
+net_init(void)
+{
+    infof("initialize...");
+    if (platform_init() == -1) {
+        errorf("platform_init() failure");
+    }
+    infof("success");
+    return 0;
+}
+
+int
+net_run(void)
+{
+    infof("startup...");
+    if (platform_run() == -1) {
+        errorf("platform_run() failure");
+        return -1;
+    }
+    infof("success");
+    return 0;
+}
+
+int
+net_shutdown(void)
+{
+    infof("shutting down...");
+    if (platform_shutdown() == -1) {
+        warnf("platform_shutdown() failure");
+    }
+    infof("success");
+    return 0;
+}
