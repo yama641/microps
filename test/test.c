bff8dbd389219f2720e6a1a96ebfc37c788ed54f
Shohei Yamamoto <yamamon8929@gmail.com>
2026-04-18 17:07:26 +0000

    Completion of step1

--- test/test.c
+++ test/test.c
@@ -10,6 +10,29 @@
 #include "test.h"
 
 static volatile sig_atomic_t terminate;
+static struct net_device *dev;
+
+struct net_device *
+dummy_init(void)
+{
+    struct net_device *dev;
+
+    dev = net_device_alloc();
+    if (!dev) {
+        errorf("net_deivce_alloc() failure");
+        return NULL;
+    }
+    dev->type = NET_DEVICE_TYPE_DUMMY;
+    dev->mtu = 128;
+    dev->hlen = 0;
+    dev->alen = 0;
+    if (net_device_register(dev) == -1) {
+        errorf("net_device_register() failure");
+        return NULL;
+    }
+    infof("success, dev=%s", dev->name);
+    return dev;
+}
 
 static void
 on_signal(int signum)
@@ -31,6 +54,11 @@ setup(void)
     infof("setup protocol stack...");
     if (net_init() == -1) {
         errorf("net_init() failure");
+        return -1;
+    }
+    dev = dummy_init();
+    if (!dev) {
+        errorf("dummy_init() failure");
         return -1;
     }
     if (net_run() == -1) {
@@ -56,6 +84,10 @@ app_main(void)
 {
     debugf("press Ctrl+C to terminate");
     while (!terminate) {
+        if (net_device_output(dev, 0x0800, test_data, sizeof(test_data), NULL) == -1) {
+            errorf("net_device_output() failure");
+            break;
+        }
         sleep(1);
     }
     debugf("terminate");
